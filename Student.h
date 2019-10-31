99e0c7b17347df86fab631ed0fec8bc870b4dc4f
RepinEA <43388644+RepinEA@users.noreply.github.com>
2019-10-31 14:24:52 +0300

    Update Student.h

--- Student.h
+++ Student.h
@@ -11,18 +11,18 @@ class Student
 {
 public:
 	Student(std::string fname,std::string iname,std::string oname,int mark);//конструктор 
-	std::string getFName();//имя
-	std::string getIName();//фамилия
-	std::string getOName();//отчество
+	std::string getFName();//получить имя
+	std::string getIName();//получить фамилия
+	std::string getOName();//получить отчество
 	void addGroup(Group& group);//добавить группу в *группы* студента
 	void removeGroup(Group& group);//удалить группу из *групп* студента
 	std::vector<Group*> getGroups();//получить группы студента
 	int getMark();//успеваемость студента
 private:
 	std::vector<Group*> groups;//группы студента
-	std::string  FName;//получить имя студента
-	std::string  IName;//получить фамилию студента
-	std::string  OName;//получить отчество студента
+	std::string  FName;//имя студента
+	std::string  IName;//фамилию студента
+	std::string  OName;//отчество студента
 	int mark;//получить успеваемость студента
 };
 
