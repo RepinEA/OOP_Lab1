b5099fc40d670734ba3680c3f589537c81549e2a
RepinEA <43388644+RepinEA@users.noreply.github.com>
2019-10-31 20:14:30 +0300

    Update StudentGroup.h

--- StudentGroup.h
+++ StudentGroup.h
@@ -1,25 +1,28 @@
-﻿#pragma once
+#pragma once
 
 #include "Student.h"
 #include <vector>
 #include <iterator>
 #include <algorithm>
 
+enum sortsTypes {byMarks,byLexicographic};//типы сортировок для функции sortStudents
+
 class Student;
+
 
 class Group
 {
 private: 
 	int Number;//номер группы
-	std::vector<Student*> students;//студенты группы
+	std::vector<Student*> students;//студенты групп
 public:
 	Group(int number);//конструктор
 	std::vector<Student*> getStudents();//получить студентов группы
 	bool addStudent(Student& student);//добавить студента в группу
 	int getNumber();//получить номер группы
 	bool tryToRemoveStudent(Student& student);//удалить студента из группы
-	Student& getStudentByName(std::string FName,std::string IName,std::string OName);//получить студента по ФИО
-	void sortStudents(std::string sort);//сортировка студентов
+	Student& getStudentByName(const std::string& FName,const std::string& IName,const std::string& OName);//получить студента по ФИО
+	void sortStudents(sortsTypes sort);//сортировка студентов
 };
 
 std::ostream& operator<<(std::ostream& stream, Group& group);//перегрузка оператора вывода
